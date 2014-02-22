4ca1e08db401b20c8badaa3476ffa99c3bdff3df
CsKim <oncelover@gmail.com>
2014-02-23 04:32:28 +0900

    Monster Controller 추가. 아직 구현 전부 되지 않음

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 import re
 
 import model.Monster
+import controller.MonsterController
+import _mysql
 
 if __name__ == "__main__":
 	#m = model.Monster.Monster();
@@ -12,7 +14,8 @@ if __name__ == "__main__":
 	#m.showData();
 	m = model.Monster.Monster.get_charactor_info_from_json(891);
 	m.showData()
+	controller.MonsterController.moster_insert_data(m);
 	#for i in range(1,300,3):
 	#	m.get_charactor_info_from_url(i);
 	#m.get_charactor_info_from_url(151);
-	raw_input();
+	raw_input();	
